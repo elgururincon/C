352229c4723e8233c55e595e231ed57c0dac4af4
elgururincon <davidrincont@gmail.com>
2017-04-01 21:28:58 -0500

    Create test1.c

---
+++ test1.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int minimo = Math.abs (A[0]-A[1]);
+
+for( i=0 ; i<n ; i++ )
+{
+  for ( j=0 ; j<n ; j++ )
+  {
+    if (i!=j)
+    {
+      distancia = Math.abs (A[0] - A[1]);
+      if ( distancia < minimo )
+      {
+        minimo = distancia
+      }
+    }
+  }
+}
+
