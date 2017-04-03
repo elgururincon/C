c087f3a17fcc1641fb8ae72cdcdf35adf3cec6f3
elgururincon <davidrincont@gmail.com>
2017-04-03 17:40:29 -0500

    Update test1.c

--- test1.c
+++ test1.c
@@ -1,19 +1,19 @@
 #include <stdio.h>
+int Main{
+  int minimo = Math.abs (A[0]-A[1]);
 
-int minimo = Math.abs (A[0]-A[1]);
-
-for( i=0 ; i<n ; i++ )
-{
-  for ( j=0 ; j<n ; j++ )
+  for( i=0 ; i<n ; i++ )
   {
-    if (i!=j)
+    for ( j=0 ; j<n ; j++ )
     {
-      distancia = Math.abs (A[0] - A[1]);
-      if ( distancia < minimo )
+      if (i!=j)
       {
-        minimo = distancia
+        distancia = Math.abs (A[0] - A[1]);
+        if ( distancia < minimo )
+        {
+          minimo = distancia
+        }
       }
     }
   }
 }
-
